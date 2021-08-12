88684915916fb45b47fc27342ade9f69157a9bac
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-08-12 12:42:32 +0200

    Add not found view

---
+++ db.js
@@ -0,0 +1,18 @@
+let db = {
+  posts: [
+    {
+      id: '1',
+      title: 'Title',
+    },
+    {
+      id: '2',
+      title: 'Title1',
+    },
+    {
+      id: '3',
+      title: 'Title2',
+    }
+  ]
+};
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoClient = require('mongodb').MongoClient;
 
 const postsRoutes = require('./routes/posts.routes');
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    console.log('Successfully connected to the database');
+    const db = client.db('BulletinBoard');
+    const app = express();
 
-app.use('/api', postsRoutes);
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
-})
+    app.use('/api', postsRoutes);;
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+    app.use((req, res) => {
+      res.status(404).send({ message: 'Not found...' });
+    })
+
+    app.listen('8000', () => {
+      console.log('Server is running on port: 8000');
+    });
+  }
 });
